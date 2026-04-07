2d90e2ce248343de8790c9c606e666c408ecbed7
shubashdutta <141384565+shubashdutta@users.noreply.github.com>
2026-04-07 11:40:48 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,26 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: false },
-  env: {
-    NEXT_PUBLIC_API_URL: "http://localhost:9999",
-    NEXT_PUBLIC_FIREBASE_KEY: "fake-key",
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "firebasestorage.googleapis.com",
+      "lh3.googleusercontent.com",
+      "res.cloudinary.com",
+    ],
   },
-  webpack: (config) => {
-    config.resolve.alias = {};
-    config.resolve.modules = [];
-    return config;
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
   },
 };
+
 module.exports = nextConfig;
